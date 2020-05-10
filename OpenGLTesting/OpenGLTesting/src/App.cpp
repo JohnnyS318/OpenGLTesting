310d9f258734dfa9c84383870ee7832c387932fa
Jonas Schneider <jonas.max.schneider@gmail.com>
2020-05-10 14:41:42 +0200

    Projection further explained

--- OpenGLTesting/OpenGLTesting/src/App.cpp
+++ OpenGLTesting/OpenGLTesting/src/App.cpp
@@ -13,6 +13,9 @@
 #include "Shader.h"
 #include "Texture.h"
 
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
   int main(void)
 {
     GLFWwindow* window;
@@ -47,10 +50,10 @@
 
     {
         float positions[] = {
-            0.5f, 0.5f, 0.0f, 0.0f,
-			0.5f, -0.5f, 1.0f, 0.0f,
-			-0.5f, -0.5f, 0.0f, 1.0f,
-			-0.5f, 0.5f, 1.0f, 1.0f,
+            100.0f, 100.0f, 0.0f, 0.0f,
+			200.0f, 100.0f, 1.0f, 0.0f,
+			200.0f, 200.0f, 0.0f, 1.0f,
+			100.0f, 200.0f, 1.0f, 1.0f,
         };
 
         unsigned int indicies[] = {
@@ -70,10 +73,13 @@
 
 		IndexBuffer ib(indicies, 6);
 
+        glm::mat4 proj = glm::ortho(0.0f, 640.0f, 0.0f, 480.0f, -1.0f, 1.0f);
+
         Shader shader("res/shaders/basic.shader");
         shader.Bind();
         const char* colorName= "u_Color";
     	shader.SetUniform4f(colorName, 0.8F, 0.3f, 0.8f, 1.0f);
+        shader.SetUniformMat4f("u_MVP", proj);
 
         Texture texture("res/textures/logo.png");
         texture.Bind();
